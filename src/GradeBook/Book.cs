a0f79a10868aafb06acfe7bce32c92bf5caf983c
Juan Pablo <juan_pal19@hotmail.com>
2020-03-16 16:40:07 -0500

    Se agrega clase ShowStats para reducir codigo

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -15,6 +16,29 @@ namespace GradeBook
         public void AddGrade(double grade)
         {
             grades.Add(grade);
+        }
+
+        ///Show Statitistics method.
+        public void ShowStatistics()
+        {
+             // var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            // grades.Add(56.1);
+
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            
+
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
         }
 
         ///Field grades.
